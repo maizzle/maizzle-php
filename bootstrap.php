864829c8ba2dda30ce1f0ca6fc14ea4c3894f62b
Cosmin Popovici <hellocosmin@gmail.com>
2018-11-12 17:08:01 +0200

    Add support for multiple collections.

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,9 @@
 /** @var $jigsaw \TightenCo\Jigsaw\Jigsaw */
 
 $events->afterBuild(function ($jigsaw) {
-    $collection = json_encode($jigsaw->getCollection('emails')->toJson());
     $config = json_encode($jigsaw->getConfig()->toJson());
-    exec("node tasks/js/after-jigsaw --collection=$collection --config=$config 2>&1", $out, $err);
+    $jigsaw->getCollections()->each(function ($item, $key) use ($config) {
+        $collection = json_encode($item->toJson());
+        exec("node tasks/js/after-jigsaw --collection=$collection --config=$config 2>&1", $out, $err);
+    });
 });
